166187e414e955dbb70337ec4a16259dea1ec215
Miljan Gicic <miljancode@gmail.com>
2023-01-26 13:07:03 +0100

    init

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,17 @@
-import './globals.css'
+import '@styles/globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&family=Poppins:wght@200;300;400;500;600;700;800&family=Volkhov:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
